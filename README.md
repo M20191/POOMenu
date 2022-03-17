94882f06e9ad372d81f022f2a3309a093ce8fb6b
fakeuser <fakemail@mail.com>
2022-03-17 12:35:04 -0300

    17/3/22

---
+++ README.md
@@ -0,0 +1,28 @@
+```ruby
+                                            .d8888b. 8888888 
+					   d88P  Y88b  888   
+					   888    888  888   
+					   888         888   
+					   888         888   
+					   888    888  888   
+					   Y88b  d88P  888   
+					    "Y8888P" 8888888 
+                   
+                                          Interactive Console
+                                             Python Edition
+				          Full console support
+```
+
+<p align=center>
+<a href="#"><img title="build" src="https://img.shields.io/badge/build-process-red?style=for-the-badge&logo=github"><a>
+</p>
+<p align="center">
+<a href="#"><img title="windows" src="https://img.shields.io/badge/OS-Windows-blue?style=for-the-badge&logo=windows"><a>
+<a href="#"><img title="windows" src="https://img.shields.io/badge/OS-Linux-blue?style=for-the-badge&logo=linux"><a>
+</p>
+<p align="center">
+  <a href="#"><img src="https://img.shields.io/badge/python-3.5%20%7C%203.6%20%7C%203.7%20%7C%203.8%20%7C%203.9%20%7C%203.10-blue?style=flat-square&logo=python"></a>
+</p>
+<p align="center">
+	<a href="#"><img title="license" src="https://img.shields.io/github/license/M20191/MSD-X?style=flat-square&logo=sublime-text"></a>
+</p>
---
+++ menu_static/action_cmd_ud.py
@@ -0,0 +1,48 @@
+from pynput import keyboard
+import os
+import time
+from colorama import *
+
+
+# First interactive model Extract a function from a file
+menu_index_option = 0
+menu_select_options = {
+    1: ["Function 1", "echo Nais! :D",],
+    2: ["Function 2", "echo Hello!",]
+    # More options... ["Display name, command on CMD"]
+    # The command can be both linux and windows, it depends on what you want to run  
+}
+
+
+def key_pressure(key):
+    try:
+        global menu_index_option
+        global menu_select_options
+        os.system("cls")
+
+        if key == keyboard.Key.up:
+            menu_index_option += 1
+                
+        elif key == keyboard.Key.down:
+            menu_index_option -= 1
+        
+        elif key == keyboard.Key.enter:
+            importing = menu_select_options[menu_index_option][1]
+            os.system(f"{importing}")
+            time.sleep(5)
+            os.system("cls")
+            
+        elif key == keyboard.Key.esc:
+            return False
+            # Exit the program
+
+        print(f"Y88-888-888-888-888-888-888-888-888-88Y\n\t\t     ↑\n\n\t\t {menu_select_options[menu_index_option][0]}\n\n\t\t     ↓\nY88-888-888-888-888-888-888-888-888-88Y")
+        
+    except:
+        menu_index_option = 0
+        print(f"Y88-888-888-888-888-888-888-888-888-88Y\n\n\t<<< Value out of range >>>\n\t\tESC FOR EXIT\nY88-888-888-888-888-888-888-888-888-88Y")
+
+# Key listener
+if __name__ == "__main__":
+    with keyboard.Listener(key_pressure) as listener:
+        listener.join()
